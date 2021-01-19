1a736ecc587452713a87767a599ae62310c81859
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-19 21:32:21 +0100

    start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #FFF;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
 function App() {
+
+  //consultar API
+  const consultarAPI = () => {
+    console.log('consultando...');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
