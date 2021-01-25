f5bbdf2771155308b54db9909b00509e2b5b0ae8
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-25 09:36:34 +0100

    add favicon

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   const [ frase, guardarFrase ] = useState({});
 
   const consultarAPI = async () => {
-    const api = await (await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes'));
+    const api = await (await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes'));
     const frase = await api.json();
     guardarFrase(frase[0]);
   };
